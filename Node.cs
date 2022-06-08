f79d3788f611da3bbabc9a947289573f9c96670d
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-08 20:49:19 +0530

    created linkedlist

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14LinkedList
+{
+    public class Node
+    {
+        public int data;         // elements added here
+        public Node next;        // next element address stored here
+        
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[]args)
         {
             Console.WriteLine("LinkedList Problem");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
