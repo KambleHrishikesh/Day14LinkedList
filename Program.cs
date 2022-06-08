db6a77403969a791813dc7b4508636bed3f24e18
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-08 21:03:51 +0530

    added 70 first

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@
         {
             Console.WriteLine("LinkedList Problem");
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.Add(30);
+            list.Add(56);
             list.Display();
         }
     }
